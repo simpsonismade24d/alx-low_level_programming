f523b41c9e04214c3f255f204d1da2d9eb9ce06a
simpsonismade24d <simpsonismade@gmail.com>
2022-09-13 02:58:19 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 
+ *
+ * Return Zero (0) always
+ */
+void print_alphabet_x10(void)
+{
+int alphabet;
+int count;
+
+
+count = 0;
+while (count < 10)
+{
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+count++;
+_putchar('\n');
+}
+
+
+}
