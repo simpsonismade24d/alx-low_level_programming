bd53f926b7280b5c4cabb1466de2955ca2557e13
simpsonismade24d <simpsonismade@gmail.com>
2022-09-14 15:08:33 -0700

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
 /**
-*main - Entry point
-*Return: Always 0 (Success)
+*main - main function
+*
+*Return: always 0
 */
 int main(void)
 {
+int a;
+int b = 0;
+
+for (a = 0; a < 1024; a++)
+{
+if (a % 3 == 0 || a % 5 == 0)
+{
+b += a;
+}
+
+}
+printf("%d\n", b);
 return (0);
 }
