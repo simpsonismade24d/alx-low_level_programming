ef009b6a871d4726d15b21da860ca9a32fc60b12
simpsonismade24d <simpsonismade@gmail.com>
2022-09-14 03:11:25 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,47 +1,40 @@
+#include <unistd.h>
 #include "main.h"
+
 /**
- *  * times_table -prints tables
- *   *
- *    * Return: Always 0.
- *     */
+ * times_table - This function prints the 9 times table
+ * starting with 0
+ */
 void times_table(void)
 {
-int a;
-int b;
-int c;
-for (a = 0; a <= 9; a++)
+
+int x, y, a, b;
+
+for (x = 0; x < 10; x++)
 {
-for (b = 0; b <= 9; b++)
+for (y = 0; y < 10; y++)
 {
-c = a * b;
-if ((c / 10) == 0)
-{
-if (b == 0)
-{
-_putchar ('0');
-}
-if (b != 0)
-{
-_putchar (' ');
-_putchar ((c % 10) + '0');
-}
-if (b < 9)
+a = (x * y) / 10;
+b = (x * y) % 10;
+
+if ((x * y) > 9)
+_putchar(a + '0');
+
+_putchar(b + '0');
+
+/* Don't print commas after the 10th column */
+if (y != 9)
 {
 _putchar(',');
-_putchar (' ');
+_putchar(' ');
+}
+
+/* Add an extra space if printing single digits */
+if (y == 9 && x == 0)
+break;
+else if (x * (y + 1) < 10)
+_putchar(' ');
+ }
+_putchar('\n');
 }
 }
-else
-{
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-}
-_putchar ('\n');
-}
-}}
