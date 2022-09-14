4d3898cbab30f858a420f29210448eb0cdcd4576
simpsonismade24d <simpsonismade@gmail.com>
2022-09-14 15:01:17 -0700

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,30 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
-#include "main.h"
 /**
-* main - main function
-* Return: 0
+*main - Entry point
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-int counter = 2;
-long int a = 1;
-long int b = a + 1;
-long int c = a + b;
-printf("%ld, %ld, ", a, b);
-while (counter < 50)
-{
-printf("%ld", c);
-counter++;
-a = b;
-b = c;
-c = a + b;
-if (counter < 50)
-{
-printf(", ");
-}
-}
-printf("\n");
 return (0);
 }
