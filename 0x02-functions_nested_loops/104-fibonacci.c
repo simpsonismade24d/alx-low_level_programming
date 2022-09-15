16d60347cec23d026766b41ea05d625060e91031
simpsonismade24d <simpsonismade@gmail.com>
2022-09-14 17:26:28 -0700

    Correction of 16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,12 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 /**
-*main - main function
-*Return: nothing
+*main - Entry point
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-int counter = 2;
-float a = 1;
-float b = a + 1;
-float c = a + b;
-printf("%.0f, ", a);
-printf("%.0f, ", b);
-while (counter < 98)
-{
-counter++;
-printf("%.0f", c);
-a = b;
-b = c;
-c = a + b;
-if (counter < 98);
-/* print result below */
-{
-printf(",");
-}
-}
-printf("\n");
+
 return (0);
 }
