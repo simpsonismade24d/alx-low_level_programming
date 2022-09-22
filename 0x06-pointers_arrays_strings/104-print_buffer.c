9a731a2a1f76ce8b9e26866d7dfd0d5dd0c50cee
simpsonismade24d <simpsonismade@gmail.com>
2022-09-22 09:47:23 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,79 +1,50 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
-* isPrintableASCII - determines if n is a printable ASCII char
-* @n: integer
-* Return: 1 if true, 0 if false
+/**
+* print_buffer - prints buffer
+* @b: buffer
+* @size: size
+* Return: void
 */
-int isPrintableASCII(int n)
+
+void print_buffer(char *b, int size)
 {
-return (n >= 32 && n <= 126);
+int o, j, i;
+
+o = 0;
+
+if (size <= 0)
+{
+printf("\n");
+return;
 }
-
-/*
-* printHexes - print hex values for string b in formatted form
-* @b: string to print
-* @start: starting position
-* @end: ending position
-*/
-void printHexes(char *b, int start, int end)
+while (o < size)
 {
-int i = 0;
-
-while (i < 10)
+j = size - o < 10 ? size - o : 10;
+printf("%08x: ", o);
+for (i = 0; i < 10; i++)
 {
-if (i < end)
-printf("%02x", *(b + start + i));
+if (i < j)
+printf("%02x", *(b + o + i));
 else
 printf("  ");
 if (i % 2)
+{
 printf(" ");
-i++;
 }
 }
-
-/*
-* printASCII - print ascii values for string b,
-* formatted to replace nonprintable chars with '.'
-* @b: string to print
-* @start: starting position
-* @end: ending position
-*/
-void printASCII(char *b, int start, int end)
+for (i = 0; i < j; i++)
 {
-int ch, i = 0;
+int c = *(b + o + i);
 
-while (i < end)
+if (c < 32 || c > 132)
 {
-ch = *(b + i + start);
-if (!isPrintableASCII(ch))
-ch = 46;
-printf("%c", ch);
-i++;
+c = '.';
 }
+printf("%c", c);
 }
-
-/*
-* print_buffer - prints a buffer
-* @b: string
-* @size: size of buffer
-*/
-void print_buffer(char *b, int size)
-{
-int start, end;
-
-if (size > 0)
-{
-for (start = 0; start < size; start += 10)
-{
-end = (size - start < 10) ? size - start : 10;
-printf("%08x: ", start);
-printHexes(b, start, end);
-printASCII(b, start, end);
 printf("\n");
+o += 10;
 }
-}
-else printf("\n");
-return (0);
 }
