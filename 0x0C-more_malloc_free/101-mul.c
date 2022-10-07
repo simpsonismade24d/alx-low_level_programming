bb839b4d73f0eadcae6f5719eca1308fcd08faaa
simpsonismade24d <simpsonismade@gmail.com>
2022-10-07 02:55:56 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,7 +7,7 @@
 * @l: size of string
 *
 * Return: void
-*/
+ */
 void _print(char *str, int l)
 {
 int i, j;
@@ -119,7 +119,7 @@ _putchar(e[ti]);
 exit(98);
 }
 for (l1 = 0; argv[1][l1]; l1++)
-		;
+;
 for (l2 = 0; argv[2][l2]; l2++)
 ;
 ln = l1 + l2 + 1;
